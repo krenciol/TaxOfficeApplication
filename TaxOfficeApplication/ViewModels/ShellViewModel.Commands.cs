1e001a5b4d1230ef4637d2f63aed5c7056cc9c0f
Paweł Jaroszek <pjaroszek@gmail.com>
2018-11-11 21:43:43 +0100

    Dodanie folderów praz pliku ze stylami

---
+++ TaxOfficeApplication/ViewModels/ShellViewModel.Commands.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Prism.Commands;
+using TaxOfficeApplication.Events;
+
+namespace TaxOfficeApplication.ViewModels
+{
+    public sealed partial class ShellViewModel
+    {
+        public ICommand ShowContractorCommand
+        {
+            get => new DelegateCommand(() =>
+            {
+                this.eventAggregator.GetEvent<NavigateToEvent>().Publish("SampleView");
+          //      this.eventAggregator.GetEvent<GetContractorsEvent>().Publish();
+            });
+        }
+    }
+}
